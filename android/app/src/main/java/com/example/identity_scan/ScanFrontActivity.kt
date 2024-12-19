3f19e0489de10495d3b978d5751472b9a962f933
Pharadon Sirijhan <64160142@go.buu.ac.th>
2024-12-19 14:21:41 +0700

    add home view

--- android/app/src/main/java/com/example/identity_scan/ScanFrontActivity.kt
+++ android/app/src/main/java/com/example/identity_scan/ScanFrontActivity.kt
@@ -2,7 +2,17 @@ package com.example.identity_scan
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ScanFrontActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
+        // Create a TextView and set some text
+        val textView = TextView(this)
+        textView.text = "Hello, Welcome to Scan Front Activity!"
+
+        // Set the TextView as the content view of the activity
+        setContentView(textView)
+    }
 }
