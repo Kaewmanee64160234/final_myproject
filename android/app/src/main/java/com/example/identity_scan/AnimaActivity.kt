15586e5df068beeda0971b8fbbcd46ed59fc2874
Pharadon Sirijhan <pharadonsiriforwork@gmail.com>
2024-12-24 08:31:50 +0700

    fix error build gradle

---
+++ android/app/src/main/java/com/example/identity_scan/AnimaActivity.kt
@@ -0,0 +1,10 @@
+package com.example.identity_scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AnimaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
