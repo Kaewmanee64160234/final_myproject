0f7c39b366ac1a4eb16e4f33c074c5679509e44d
Pharadon Sirijhan <pharadonsiriforwork@gmail.com>
2024-12-25 15:08:31 +0700

    add cameraview

---
+++ android/app/src/main/java/com/example/identity_scan/CaptureActivity.kt
@@ -0,0 +1,10 @@
+package com.example.identity_scan
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaptureActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
