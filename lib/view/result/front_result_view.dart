f07808e556f4fb6d91f85d44d065c5f872119d2c
Pharadon Sirijhan <64160142@go.buu.ac.th>
2025-01-08 16:55:13 +0700

    update ui result front card

--- lib/view/result/front_result_view.dart
+++ lib/view/result/front_result_view.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
 import 'package:identity_scan/model/front/id_card.dart';
 
@@ -11,19 +14,72 @@ class FrontResultView extends StatefulWidget {
 }
 
 class _FrontResultViewState extends State<FrontResultView> {
+  static late String profilePicString;
+
+  static Image decodeBase64ToImage(String base64String) {
+    Uint8List bytes = base64Decode(base64String);
+    // Return the image from the bytes
+    return Image.memory(bytes);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    setState(() {
+      profilePicString = widget.idCard.portrait;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Result Front"),
+        title: Text("ผลการสแกนหน้าบัตร"),
+        centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(child: Text("ID Card: ${widget.idCard.idNumber}")),
-          Center(child: Text("FullName : ${widget.idCard.th.fullName}")),
-          Center(child: Text("Address : ${widget.idCard.th.address.full}")),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (profilePicString.isNotEmpty)
+              Padding(
+                padding: const EdgeInsets.only(bottom: 16.0),
+                child: SizedBox(
+                  height: 200,
+                  child: decodeBase64ToImage(profilePicString),
+                ),
+              ),
+            Text(
+              "รหัสประจำตัว: ${widget.idCard.idNumber}",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller:
+                  TextEditingController(text: widget.idCard.th.fullName),
+              decoration: InputDecoration(
+                labelText: "ชื่อ - สกุล",
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(),
+              ),
+              readOnly: false,
+            ),
+            SizedBox(height: 16),
+            TextField(
+              controller:
+                  TextEditingController(text: widget.idCard.th.address.full),
+              decoration: InputDecoration(
+                labelText: "Address",
+                filled: true,
+                fillColor: Colors.grey[200],
+                border: OutlineInputBorder(),
+              ),
+              readOnly: false,
+            ),
+          ],
+        ),
       ),
     );
   }
