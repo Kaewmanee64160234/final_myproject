4909091a754d08ef12e905159a6123c59e063371
Pharadon Sirijhan <pharadonsiriforwork@gmail.com>
2025-01-22 15:14:03 +0700

    แก้ default font

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-
 import 'package:get/get.dart';
-
 import 'app/routes/app_pages.dart';
+import 'package:google_fonts/google_fonts.dart';  
 
 
 Future<void> main() async {
@@ -14,6 +13,10 @@ Future<void> main() async {
       title: "Application",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      theme: ThemeData(
+        // กำหนดฟอนต์ให้กับ TextTheme ของแอป
+      fontFamily: GoogleFonts.kanit().fontFamily
+      ),
     ),
   );
 }
